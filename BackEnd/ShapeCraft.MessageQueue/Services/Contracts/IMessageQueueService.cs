67c9ff29d056b91df9544c58d502e6842020c236
Arnold eth <arne.meersman12@gmail.com>
2025-08-23 20:29:57 +0200

    feature: Add message queue functionality for indexing documents

---
+++ BackEnd/ShapeCraft.MessageQueue/Services/Contracts/IMessageQueueService.cs
@@ -0,0 +1,15 @@
+﻿using ShapeCraft.MessageQueue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCraft.MessageQueue.Services.Contracts
+{
+    public interface IMessageQueueService
+    {
+        Task SendMessageAsync(string message);
+        Task<QueueMessage?> ReceiveQueueMessageAsync();
+    }
+}
