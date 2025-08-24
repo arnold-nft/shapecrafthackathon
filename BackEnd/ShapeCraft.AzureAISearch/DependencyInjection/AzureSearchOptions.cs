44b42035f7f9b1465d25b716b62d804e808b68c9
Arnold eth <arne.meersman12@gmail.com>
2025-08-24 09:23:27 +0200

    feature: Implement Azure AI search

---
+++ BackEnd/ShapeCraft.AzureAISearch/DependencyInjection/AzureSearchOptions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShapeCraft.AzureAISearch.Services;
+using ShapeCraft.AzureAISearch.Services.Contracts;
+using ShapeCraft.Core.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace ShapeCraft.AzureAISearch.DependencyInjection
+{
+    public static class AzureSearchAIOptions
+    {
+
+        public static IServiceCollection AddAzureSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+               .AddConfiguration<AzureSearchOptions>(options =>
+               {
+                   configuration.GetSection("SearchAIOptions").Bind(options);
+               });
+
+
+            services.AddSingleton<ISearchIndexService, SearchIndexService>();
+
+            services.AddSingleton<IVectorSearchService, VectorSearchService>();
+
+            services.AddSingleton<IRagChatService, RagChatService>();
+
+            services.AddSingleton<IIngestionService, IngestionService>();
+
+            return services;
+        }
+    }
+}
